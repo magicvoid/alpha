2e0a9fcb0f7c40e33b8a3cea14822868db29391a
Zzuizuo <Zzuizuo3094720053@163.com>
2016-12-28 21:08:42 +0800

    index
    首页

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,27 +1,89 @@
 //index.js
 //获取应用实例
-var app = getApp()
+var app = getApp();
+var url = 'https://test.genwoshua.com/alpha/api/article/query';
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    mode: 'center',
-    src: '/images/article_sample.jpg'
-  },
+   data: {
+      articles:null
+    }, 
+   onReady: function () {
+    //初始加载
+    refresh(this);
+  } ,
+    
+    onPullDownRefresh: function() {
+        // Do something when pull down.
+        console.log('刷新');
+    },
+    onReachBottom: function() {
+        // Do something when page reach bottom.
+        refresh(this);
+    },
+
+    area:function(){
+  	    wx.showModal({
+            title: '提示',
+            content: '暂时只支持成都地区',
+            success: function(res) {
+                if (res.confirm) {
+                console.log('用户点击确定')
+                }
+            }
+        })
+    },
+    detailLoad:function(){
+
+    },
+    toDetail:function(event){
+      //  var url = event.currentTarget.dataset.article.origin;
+      //  var that = this;
+      // console.log(url)
+    },
   //事件处理函数
   bindViewTap: function() {
     console.log('bindViewTap')
-  },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
   }
-  
 })
+
+function refresh(that){
+ var queryObject = new Object({
+   size:3
+ });
+ console.log(that.data.articles);
+      if(that.data.articles!=null){
+        queryObject.fromId = that.data.articles[that.data.articles.length-1].id;
+      }
+        wx.request({
+          url: url,
+          method: 'post',
+          header: {
+            'content-type': 'application/json'
+          },
+          data:queryObject,
+          success: function(res) {
+            var articles = res.data.content;
+            var lastPage = res.data.lastPage;//根据页码判断是否显示
+            //console.log(articles)
+            var newArticles = that.data.articles;
+                if(newArticles == null){
+                  newArticles = articles;
+                }
+            for(let index in articles){
+                var article = articles[index];
+                if(article.tags){
+                  article.tags = article.tags.split(',');
+                }else{
+                  article.tags =""
+                }
+                if(that.data.articles!=null){
+                  newArticles.push(article);
+                }
+              }
+              that.setData({articles:newArticles})
+            if(lastPage){
+              console.log('没有更多消息了')
+            }
+            
+          }
+       })
+}
